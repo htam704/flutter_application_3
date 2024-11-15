2e699dd115a480df14c3d265622b001f9851e317
Hikari Tamura <jinnsei.81@gmail.com>
2024-11-15 09:08:00 +0100

    personalizar_imagen_uso_container

--- lib/widgets/custom_card_tipo_2.dart
+++ lib/widgets/custom_card_tipo_2.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class CustomCardTipo2 extends StatelessWidget {
@@ -5,14 +7,22 @@ class CustomCardTipo2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
+    return Card(
       child: Column(
         children: [
-          FadeInImage(
+          const FadeInImage(
             image: NetworkImage(
                 'https://wallpapers.com/images/hd/hd-nature-background-dftck70xg2lujaxb.jpg'),
             placeholder: AssetImage('assets/Loading_icon.gif'),
-          )
+            width: double.infinity, //coge todo el ancho posible
+            height: 240, // rellena con espacio
+            fit: BoxFit.cover, //rellena el espacio anterior cortando la imagen
+            fadeInDuration:
+                Duration(milliseconds: 3000), //duracion fundido carga
+          ),
+          Container(
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              child: const Text('The Nature'))
         ],
       ),
     );
