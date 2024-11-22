42b07cbd5998b43a11656ad502841794543fa0b6
Hikari Tamura <jinnsei.81@gmail.com>
2024-11-22 23:48:36 +0100

    Configurando Widget Imagen

--- lib/widgets/image_widget.dart
+++ lib/widgets/image_widget.dart
@@ -8,14 +8,28 @@ class ImageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FadeInImage(
-        image: NetworkImage(imageUrl),
-        placeholder: const AssetImage('assets/Loading_icon.gif'),
-        width: double.infinity,
-        height: 240,
-        fit: BoxFit.cover,
-        fadeInDuration: const Duration(milliseconds: 300),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20), // Bordes redondeados
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2), // Sombra suave
+            spreadRadius: 3,
+            blurRadius: 10,
+            offset: const Offset(0, 4), // Sombra hacia abajo
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: FadeInImage(
+          image: NetworkImage(imageUrl),
+          placeholder: const AssetImage('assets/Loading_icon.gif'),
+          width: double.infinity, // Ocupa todo el ancho
+          height: 240, // Ajusta la altura
+          fit: BoxFit.cover, // Ajuste de imagen
+          fadeInDuration: const Duration(milliseconds: 3000),
+        ),
       ),
     );
   }
